0ca0fe72d380c054e2df356e79371e3ed1d5ce84
Serhiy Likhanov <slikhan@gmail.com>
2022-06-20 15:20:13 -0400

    Sorted some theme management

---
+++ lua/config/colorschemes/tokyonight.lua
@@ -0,0 +1,11 @@
+-- Example config in Lua
+vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_italic_functions = false
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+
+-- Load the colorscheme
+vim.cmd[[colorscheme tokyonight]]
+
