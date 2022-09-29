9267caf2b6ba1905dc91632f2065b862b928a360
Serhiy Likhanov <slikhan@gmail.com>
2022-09-28 21:55:19 -0400

    Some treesitter configuration updates

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -4,12 +4,12 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "c", "cpp" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
+    disable = { }, -- list of language that will be disabled
     additional_vim_regex_highlighting = false,
   },
   indent = { enable = true, disable = { "yaml" } },
