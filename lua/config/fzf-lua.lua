1ecac950d15ac1fd213169ad3b706ab472b5734e
Serhiy Likhanov <slikhan@gmail.com>
2022-10-10 08:15:23 -0400

    Some tweaks, switching to FZF for lsp.

--- lua/config/fzf-lua.lua
+++ lua/config/fzf-lua.lua
@@ -9,4 +9,8 @@ vim.keymap.set("n", "<leader>f", function () fzf.files() end)
 vim.keymap.set("n", "<leader>b", function () fzf.buffers() end)
 vim.keymap.set("n", "<leader>*", function () fzf.grep_cword() end)
 vim.keymap.set("n", "<leader>C", function () fzf.colorschemes() end)
+vim.keymap.set("n", "gd", function() fzf.lsp_definitions() end)
+vim.keymap.set("n", "gD", function() fzf.lsp_declarations() end)
+vim.keymap.set("n", "gi", function() fzf.lsp_implementations() end)
+vim.keymap.set("n", "gr", function() fzf.lsp_references() end)
 
