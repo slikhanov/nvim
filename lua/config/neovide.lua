49109b9cea43522419a1736cd59dd5a41280bf98
Serhiy Likhanov <slikhan@gmail.com>
2023-02-04 21:00:50 -0500

    Separate font for windows

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,5 +1,9 @@
 if vim.g.neovide then
-  vim.opt.guifont = { "JetBrainsMono Nerd Font", "FiraCode NF" }
+  if vim.fn.has("win32") then
+    vim.opt.guifont = { "JetBrainsMono NFM" }
+  else
+    vim.opt.guifont = { "JetBrainsMono Nerd Font" }
+  end
   vim.g.neovide_refresh_rate_idle = 5 -- Refresh rate when application not in focus
   vim.g.neovide_cursor_trail_size = 0.1 
   vim.g.neovide_floating_blur_amount_x = 20.0 -- Floating window blur size x, y
