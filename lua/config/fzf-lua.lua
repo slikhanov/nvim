92d90f3a0dc3f847a566e1ed6cfc6e66375e0b0f
Serhiy Likhanov <slikhan@gmail.com>
2023-02-04 21:49:37 -0500

    Formatted some code

--- lua/config/fzf-lua.lua
+++ lua/config/fzf-lua.lua
@@ -1,13 +1,12 @@
 import('fzf-lua', function(fzf)
   -- FZF Keymaps
-  vim.keymap.set("n", "<leader>m", function () fzf.builtin() end)
-  vim.keymap.set("n", "<leader>f", function () fzf.files() end)
-  vim.keymap.set("n", "<leader>b", function () fzf.buffers() end)
-  vim.keymap.set("n", "<leader>*", function () fzf.grep_cword() end)
-  vim.keymap.set("n", "<leader>C", function () fzf.colorschemes() end)
+  vim.keymap.set("n", "<leader>m", function() fzf.builtin() end)
+  vim.keymap.set("n", "<leader>f", function() fzf.files() end)
+  vim.keymap.set("n", "<leader>b", function() fzf.buffers() end)
+  vim.keymap.set("n", "<leader>*", function() fzf.grep_cword() end)
+  vim.keymap.set("n", "<leader>C", function() fzf.colorschemes() end)
   vim.keymap.set("n", "gd", function() fzf.lsp_definitions() end)
   vim.keymap.set("n", "gD", function() fzf.lsp_declarations() end)
   vim.keymap.set("n", "gi", function() fzf.lsp_implementations() end)
   vim.keymap.set("n", "gr", function() fzf.lsp_references() end)
 end)
-
